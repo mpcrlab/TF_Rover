632b1d9463c69e25763adcb1d5b28c7b8e355a8b
root <mtetifau@gmail.com>
2017-06-28 15:36:49 +0000

    Put in simple prompt

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from RoverRun import *
 from RoverSimple import *
 if __name__ == '__main__':
-	
-	rover = RoverSimple()
-
+	a = raw_input('a for autonomous, k for keyboard')
+        if a in ['a', 'A']:
+	    rover = RoverRun()
+        else:
+            rover = RoverSimple()
