ddcbb3bdc0923c91a321df37c1ec0eaddfe58bdd
Michael A. Teti <mtetifau@gmail.com>
2017-07-13 12:06:22 -0400

    Update model_train.py

--- model_train.py
+++ model_train.py
@@ -17,23 +17,29 @@ import sys
 import os
 import cv2
 from NetworkSwitch import *
+from sklearn.preprocessing import scale
 
+
+# prompt the user to choose the name of the saved model
 print('What filename do you want to save this model as?')
 m_save = raw_input('Rover Used_number of frames/stackinterval_other parameters  ')
 
+# prompt the user for which model they want to train from NetworkSwitch.py
 model_num = np.int32(raw_input('Which model do you want to train (0 - 10)?'))
 
+# define useful variables
 os.chdir('/home/TF_Rover/RoverData')
 fnames = glob.glob('*.h5')
 epochs = 800
 batch_sz = 70
 errors = []
 test_num = 600
-f_int = 10
-f_int2 = 20
+f_int = 2
+f_int2 = 5
 val_accuracy = []
 num_stack = 3
 val_name = 'Run_2_both_lights_on.h5'
+print(fnames)
 
 
 
@@ -42,6 +48,23 @@ def add_noise(x, y):
     x = np.concatenate((x, x_aug), 0)
     y = np.concatenate((y, y), 0)
     return x, y
+
+
+def create_framestack(x, y, f_int, f_int2):
+    X_ = []
+    Y_ = []
+    for ex_num in range(x.shape[0]-1, f_int2, -1):
+        X2 = x[ex_num-f_int, :, :, :]
+        X3 = x[ex_num-f_int2, :, :, :]
+        X_.append(np.concatenate((x[ex_num, :, :, :], X2, X3), 2))
+        Y_.append(y[ex_num, :])
+    return np.assarray(X_), np.asarray(Y_)
+
+
+def feature_scale(x):
+    x = scale(x.reshape([x.shape[0], -1])), 1)
+    return x.reshape([x.shape[0], 130, 320, 1])
+
 
 
 # Validation set
@@ -58,32 +81,24 @@ ty = np.zeros([test_num, 3])
 ty[np.arange(test_num), y_[:test_num]] = 1.
 
 # Feature Scaling validation data
-tx = np.transpose(tx.reshape([test_num, -1]))
-tx = (tx-np.mean(tx, 0))/(np.std(tx, 0)+1e-6)
-tx = np.reshape(tx.transpose(), [test_num, 130, 320, 1])
+tx = feature_scale(tx)
 
 # Create validation framestack
-TX = []
-TY = []
-for i in range(test_num-1, f_int2, -1):
-    TX2 = tx[i-f_int, :, :, :]
-    TX3 = tx[i-f_int2, :, :, :]
-    TX.append(np.concatenate((tx[i, :, :, :], TX2, TX3), 2))
-    TY.append(ty[i, :])
-
-TX = np.asarray(TX)
-TY = np.asarray(TY)
-print(TX.shape)
-print(TY.shape)
+TX, TY = create_framestack(tx, ty, f_int, f_int2)
 assert(TY.shape[0] == TX.shape[0]),'data and label shapes do not match'
 
+
+# Create input layer and label placeholder for the network
 labels = tf.placeholder(dtype=tf.float32, shape=[None, 3])
 network = tf.placeholder(dtype=tf.float32, shape=[None, 130, 320, num_stack])
 
 
+# send the input placeholder to the specified network
 net_out = modelswitch[model_num](network)
 acc = tf.reduce_mean(tf.to_float(tf.equal(tf.argmax(net_out, 1), tf.argmax(labels, 1))))
-cost = categorical_crossentropy(net_out, labels)
+cost = categorical_crossentropy(net_out, labels) # crossentropy loss function
+
+# gradient descent optimizer
 opt = tf.train.AdamOptimizer(learning_rate=0.0001)
 trainop = tflearn.TrainOp(loss=cost,
                          optimizer=opt,
@@ -96,11 +111,12 @@ for i in range(epochs):
     # pick random dataset for this epoch
     n = np.random.randint(1, len(fnames)-1, 1)
     filename = fnames[n[0]]
-    
+    # skip the validation dataset if chosen
     if filename == val_name:
         print('skipping iteration')
         continue
-        
+
+    # load the chosen data file
     f = h5py.File(filename, 'r')
     X = np.asarray(f['X'])
     y = np.int32(f['Y']) + 1
@@ -115,32 +131,21 @@ for i in range(epochs):
         if j * batch_sz + batch_sz <= (X.shape[0]-1):
             x = X[j*batch_sz:j*batch_sz+batch_sz, :, :, :]
             y = Y[j*batch_sz:j*batch_sz+batch_sz, :]
-        elif j*batch_sz + batch_sz >= (X.shape[0]-1) and X.shape[0] - (j*batch_sz + batch_sz) >= f_int2:
+        elif j*batch_sz + batch_sz >= (X.shape[0]-1) and X.shape[0]-(j*batch_sz + batch_sz)>=f_int2:
             x = X[j*batch_sz:X.shape[0], :, :, :]
             y = Y[j*batch_sz:X.shape[0], :]
         else:
             continue
 
-        # Feature Scaling
-        x = np.transpose(x.reshape([y.shape[0], -1]))
-        x = (x-np.mean(x, 0))/(np.std(x, 0)+1e-6)
-        x = np.reshape(x.transpose(), [y.shape[0], 130, 320, 1])
+        # local feature Scaling
+        x = feature_scale(x)
 
-
-        # Create framestack
-        X_ = []
-        Y_ = []
-
-        for ex_num in range(x.shape[0]-1, f_int2, -1):
-            X2 = x[ex_num-f_int, :, :, :]
-            X3 = x[ex_num-f_int2, :, :, :]
-            X_.append(np.concatenate((x[ex_num, :, :, :], X2, X3), 2))
-            Y_.append(y[ex_num, :])
-
+        # framestack
+        X_, Y_ = create_framestack(x, y, f_int, f_int2)
 
         # Data Augmentation
         X_, Y_ = add_noise(np.asarray(X_), np.asarray(Y_))
-            
+
         # Training
         model.fit_batch(feed_dicts={network:X_, labels:Y_})
         train_acc = model.session.run(acc, feed_dict={network:X_, labels:Y_})
@@ -148,15 +153,19 @@ for i in range(epochs):
                                     i+1, filename, train_acc, 1-train_acc) )
         sys.stdout.flush()
 
+    # Get validation accuracy and error rate
     val_acc = model.session.run(acc, feed_dict={network:TX, labels:TY})
     print(val_acc)
     errors.append(1.-val_acc)
     val_accuracy.append(val_acc)
+    f.flush()
     f.close()
 
+# Save model and acc/error curves
 np.save(m_save+modelswitch[model_num].__name__+'.npy', errors, val_accuracy)
 model.save(m_save+modelswitch[model_num].__name__)
 
+# Plot the validation error rate
 fig = plt.figure()
 a1 = fig.add_subplot(111)
 a1.plot(errors)
