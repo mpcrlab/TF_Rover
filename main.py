846460cf087e32263807adf8f7699105bf2e7546
root <mtetifau@gmail.com>
2017-07-14 19:24:05 +0000

    added stack

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+from RoverRunStack import *
 from RoverRun import *
 from RoverSimple import *
 if __name__ == '__main__':
 	a = raw_input('a for autonomous, k for keyboard')
         if a in ['a', 'A']:
-	    rover = RoverRun()
+            b = raw_input('Stack or no stack (y/n)? ')
+	    if b in ['Y', 'y']:
+                rover = RoverRunStack()
+            else:
+                rover = RoverRun()
         else:
             rover = RoverSimple()
