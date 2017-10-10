7e7eee398d37686408bd02934492946c0ec51d5e
Michael A. Teti <mtetifau@gmail.com>
2017-10-10 17:40:47 -0400

    Update Data.py

--- Data.py
+++ Data.py
@@ -4,10 +4,14 @@ import h5py
 import progressbar
 import datetime
 
+
+
 class Data():
     def __init__(self):
         self.angles = []
         self.images = []
+	self.start = time.time()
+
 
     def load(self):
         pass
@@ -18,14 +22,22 @@ class Data():
 
         self.angles = np.array(self.angles, dtype='float16')
 
-        save_time = datetime.datetime.now().strftime("%m%d%H%M%S")
+        elapsedTime = int(time.time() - self.start)
 
-        #dset_name = "Run_" + save_time + '.h5'
-	dset_name ='test2.h5'
+
+
+	
+        dset_name = "Run_" + str(elapsedTime) + "seconds_" + "Chris_Sheri" + ".h5"
+
 
 	h5f = h5py.File(dset_name, 'w')
 	h5f.create_dataset('X', data=self.images)
 	h5f.create_dataset('Y', data=self.angles)
 
 	h5f.close()
+
+
+	
+
+
 
