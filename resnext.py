73703f1b3231a4f210dc1c56d6d813e5073a5517
Michael A. Teti <mtetifau@gmail.com>
2018-01-26 18:07:05 -0500

    Add files via upload

--- resnext.py
+++ resnext.py
@@ -5,16 +5,16 @@ def resnext_block2(incoming, nb_blocks, out_channels, cardinality,
                   bias_init='zeros', trainable=True, restore=True,
                   reuse=False, scope=None, name="ResNeXtBlock"):
 
-    b, h, w, in_channels = incoming.get_shape().as_list()
-
+    _, h, w, in_channels = incoming.get_shape().as_list()
+    b = 50
+    out = tf.zeros([b, h, w, out_channels])
     if downsample:
         downsample_strides = 2
-        out = tf.zeros([b, int(h/2), int(w/2), out_channels])
-    else:
-        out = tf.zeros([b, h, w, out_channels])
-
-
+        out = tf.zeros([b, np.ceil(h/2.), np.ceil(w/2.), out_channels])
+        print(h/2)
+        print(w/2)
     for i in range(nb_blocks):
+        _, _, _, in_channels = incoming.get_shape().as_list()
         for j in range(cardinality):
             resnext = conv_2d(incoming, out_channels, 3,
                               downsample_strides, 'same',
@@ -41,9 +41,9 @@ def resnext_block2(incoming, nb_blocks, out_channels, cardinality,
             ch = (out_channels - in_channels)//2
             incoming = tf.pad(incoming,
                             [[0, 0], [0, 0], [0, 0], [ch, ch]])
-
+        if downsample_strides > 1:
+            incoming = avg_pool_2d(incoming, downsample_strides)
         out = out + incoming
-
         incoming = out
 
     return out
