57779eb63ff9d7f1976a5d6fce77648e3045ba6c
Michael A. Teti <mtetifau@gmail.com>
2018-02-06 18:27:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ from RoverSimple import *
 if __name__ == '__main__':
 	a = raw_input('a for autonomous, k for keyboard')
         if a in ['a', 'A']:
-            b = raw_input('Stack or no stack (y/n)? ')
-	    if b in ['Y', 'y']:
-                rover = RoverRun(framestack=[5, 15])
-            else:
-                rover = RoverRun()
+            rover = RoverRun()
         else:
             rover = RoverSimple()
