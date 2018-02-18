aca35e6bd03e33e65576885b4e4ecde11ad7a8b6
Michael A. Teti <mtetifau@gmail.com>
2018-02-18 10:36:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from RoverSimple import *
 if __name__ == '__main__':
 	a = raw_input('a for autonomous, k for keyboard')
         if a in ['a', 'A']:
-            rover = RoverRun()
+	    a2 = raw_input('do you want to learn? (y / n) ')
+	    if a2 in ['N', 'n']:
+            	rover = RoverRun()
+	    else:
+		rover = RoverRun(learn=True)
         else:
             rover = RoverSimple()
