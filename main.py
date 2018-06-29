db5128bbe6663164fdaec05cc8f09a6a91db5add
root <mtetifau@gmail.com>
2018-06-29 21:00:02 +0000

    added arguments

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 from RoverRun import *
 from RoverSimple import *
+import argparse
+
 if __name__ == '__main__':
-	a = raw_input('a for autonomous, k for keyboard')
-        if a in ['a', 'A']:
-	    a2 = raw_input('do you want to learn? (y / n) ')
-	    if a2 in ['N', 'n']:
-            	rover = RoverRun()
-	    else:
-		rover = RoverRun(learn=True)
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+    '--model_name',
+    type=str,
+    default='',
+    help='name of the model file')
+        
+    parser.add_argument(
+    '--autonomous',
+    type=str,
+    default='y',
+    help='Autonomous or human-controlled? (y/n)')
+
+        args = parser.parse_args()
+	a = args.autonomous
+        model_file_name = args.model_name
+
+        if a in ['y', 'Y']:
+            rover = RoverRun(filename=model_file_name)
         else:
             rover = RoverSimple()
