2af70e0a38c6714fc12c4ad62203e676dde067e6
root <mtetifau@gmail.com>
2018-07-09 15:19:16 +0000

    fixed indentation issue

--- main.py
+++ main.py
@@ -7,22 +7,22 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-    '--model_name',
-    type=str,
-    default='',
-    help='name of the model file')
+        '--model_name',
+    	type=str,
+    	default='',
+    	help='name of the model file')
         
     parser.add_argument(
-    '--autonomous',
-    type=str,
-    default='y',
-    help='Autonomous or human-controlled? (y/n)')
+    	'--autonomous',
+    	type=str,
+    	default='y',
+    	help='Autonomous or human-controlled? (y/n)')
 
-        args = parser.parse_args()
-	a = args.autonomous
-        model_file_name = args.model_name
+    args = parser.parse_args()
+    a = args.autonomous
+    model_file_name = args.model_name
 
-        if a in ['y', 'Y']:
-            rover = RoverRun(filename=model_file_name)
-        else:
-            rover = RoverSimple()
+    if a in ['y', 'Y']:
+        rover = RoverRun(filename=model_file_name)
+    else:
+        rover = RoverSimple()
