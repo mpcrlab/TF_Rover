23bd2ef7c7960d6ce6d47610314fcd3383c0a8ad
root <mtetifau@gmail.com>
2018-07-09 15:44:12 +0000

    fixed the arguments

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from RoverRun import *
 from RoverSimple import *
 import argparse
+from NetworkSwitch import *
+
 
 if __name__ == '__main__':
 
@@ -18,11 +20,17 @@ if __name__ == '__main__':
     	default='y',
     	help='Autonomous or human-controlled? (y/n)')
 
+    parser.add_argument(
+	'--network',
+	type=int,
+	help='Number of the network that exists in the model file.')
+
     args = parser.parse_args()
     a = args.autonomous
     model_file_name = args.model_name
+    network_name = args.network
 
     if a in ['y', 'Y']:
-        rover = RoverRun(filename=model_file_name)
+        rover = RoverRun(model_file_name, modelswitch[network_name])
     else:
         rover = RoverSimple()
